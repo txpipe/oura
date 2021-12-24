1700d5bab35cef06790751d103a7896cccc09e38
Mateusz Czeladka <mateusz.szczap@gmail.com>
2021-12-24 14:34:58 +0100

    Bug fix
    ```
    ❯ curl -X PUT http://localhost:9200/_template/oura.sink.v0 \
             -H 'Content-Type: application/json' \
             -d @index-template.json
    {"acknowledged":true}
    ```

--- testdrive/elasticsearch/setup-index.sh
+++ testdrive/elasticsearch/setup-index.sh
@@ -1,4 +1,4 @@
 
-curl -X PUT http://localhost:9200/_index_template/oura.sink.v0 \
+curl -X PUT http://localhost:9200/_template/oura.sink.v0 \
    -H 'Content-Type: application/json' \
    -d @index-template.json
