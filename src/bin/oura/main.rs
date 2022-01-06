3170615e4ac5d15db8f8b428acff3f7f3af591bb
Mark Stopka <mark.stopka@perlur.cloud>
2022-01-06 17:47:30 +0100

    fix: Update more code to new Clap API

--- src/bin/oura/main.rs
+++ src/bin/oura/main.rs
@@ -3,7 +3,7 @@ mod watch;
 
 use std::process;
 
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{App, AppSettings, Arg};
 
 type Error = oura::framework::Error;
 
@@ -12,17 +12,17 @@ fn main() {
         .name("oura")
         .about("the tail of cardano")
         .subcommand(
-            SubCommand::with_name("watch")
-                .arg(Arg::with_name("socket").required(true))
+            App::new("watch")
+                .arg(Arg::new("socket").required(true))
                 .arg(
-                    Arg::with_name("bearer")
+                    Arg::new("bearer")
                         .long("bearer")
                         .takes_value(true)
                         .possible_values(&["tcp", "unix"]),
                 )
-                .arg(Arg::with_name("magic").long("magic").takes_value(true))
+                .arg(Arg::new("magic").long("magic").takes_value(true))
                 .arg(
-                    Arg::with_name("since")
+                    Arg::new("since")
                         .long("since")
                         .takes_value(true)
                         .help(
@@ -30,15 +30,15 @@ fn main() {
                     ),
                 )
                 .arg(
-                    Arg::with_name("mode")
+                    Arg::new("mode")
                         .long("mode")
                         .takes_value(true)
                         .possible_values(&["node", "client"]),
                 ),
         )
         .subcommand(
-            SubCommand::with_name("daemon").arg(
-                Arg::with_name("config")
+            App::new("daemon").arg(
+                Arg::new("config")
                     .long("config")
                     .takes_value(true)
                     .help("config file to load by the daemon"),
