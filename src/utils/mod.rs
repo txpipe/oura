c02f9d487d209d30f8d810adba3dfd97e374c9e8
Santiago Carmuega <santiago@carmuega.me>
2022-03-08 08:58:31 -0300

    fix: Fix testnet well-known time parameters

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -84,9 +84,9 @@ impl ChainWellKnownInfo {
     pub fn testnet() -> Self {
         ChainWellKnownInfo {
             byron_slot_length: 20,
-            byron_known_slot: 1031,
-            byron_known_time: 1564020236,
-            byron_known_hash: "388a82f053603f3552717d61644a353188f2d5500f4c6354cc1ad27a36a7ea91"
+            byron_known_slot: 0,
+            byron_known_time: 1564010416,
+            byron_known_hash: "8f8602837f7c6f8b8867dd1cbc1842cf51a27eaed2c70ef48325d00f8efb320f"
                 .to_string(),
             shelley_slot_length: 1,
             shelley_known_slot: 1598400,
