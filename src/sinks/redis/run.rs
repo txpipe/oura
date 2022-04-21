1efadc953da07b5c6dae57f64915eea8d8a0ac7e
tp <torben.poguntke@iu-study.org>
2022-04-21 12:34:15 +0200

    addres RedisRecordType

--- src/sinks/redis/run.rs
+++ src/sinks/redis/run.rs
@@ -1,7 +1,28 @@
 #![allow(unused_variables)]
 use std::sync::Arc;
+use serde::Serialize;
 use serde_json::{json,Value};
 use crate::{pipelining::StageReceiver, utils::Utils, Error, model::Event, model::EventData};
+
+#[derive(Serialize)]
+pub struct RedisRecord {
+    pub event: Event, 
+    pub variant: String,
+    pub key: Option<String>,
+}
+
+impl From<Event> for RedisRecord {
+    fn from(event: Event) -> Self {
+        let variant = event.data.to_string().to_lowercase();
+        let key = make_key(&event);
+        RedisRecord {
+            event,
+            variant,
+            key,
+        }
+    }
+    
+}
 
 pub fn producer_loop(
     input       : StageReceiver,
@@ -10,132 +31,131 @@ pub fn producer_loop(
 ) -> Result<(), Error> {
     for event in input.iter() {
         utils.track_sink_progress(&event);
-        let value = json!(&event);
-        let key = make_key(&event)?;
-        log::info!("Key: {:?}, Event: {:?}",key,event);
-        if let Some(k) = key{
+        let value = RedisRecord::from(event);
+        log::info!("Variant: {:?}, Key: {:?}, Event: {:?}", value.variant, value.key, value.event);
+        if let Some(k) = value.key{
             //Needs minimum redis 6.9 (release canidate 7.0)
-            let _ : () = redis::cmd("XADD").arg(event.data.to_string().to_lowercase()).arg("*").arg(&[(k,Value::to_string(&value))]).query(conn)?;
+            let _ : () = redis::cmd("XADD").arg(value.variant).arg("*").arg(&[(k,json!(value.event).to_string())]).query(conn)?;
         }
     }
     Ok(())
 }
 
-fn make_key(event :  &Event) -> Result<Option<String>,Error> {
+fn make_key(event :  &Event) -> Option<String> {
     match event.data.clone() {
         EventData::Block(_) => {
-            Ok(event.context.block_number.map(|n| n.to_string()))
+            event.context.block_number.map(|n| n.to_string())
         }
 
         EventData::BlockEnd(_) => {
-            Ok(None)
+            None
         },
 
         EventData::Transaction(_) => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::TransactionEnd(_) => {
-            Ok(None)
+            None
         },
 
         EventData::TxInput(tx_input_record) => {
-            Ok(Some(tx_input_record.tx_id +"#"+&tx_input_record.index.to_string()).map(|n| n.to_string()))
+            Some(tx_input_record.tx_id +"#"+&tx_input_record.index.to_string()).map(|n| n.to_string())
         },
 
         EventData::TxOutput(_) => {
             let mut key = match event.context.tx_hash.clone(){
                 Some(hash) => hash,
-                None => return Err("no txhash found".into()),
+                None => return None,
             };
             if let Some(outputindex) = event.context.output_idx {
                 key = key + "#" + &outputindex.to_string()
             }
 
-            Ok(Some(key))
+            Some(key)
         },
 
         EventData::OutputAsset(output_asset_record) => {
-            Ok(Some(output_asset_record.policy.clone()+"."+&output_asset_record.asset).map(|n| n.to_string()))
+            Some(output_asset_record.policy.clone()+"."+&output_asset_record.asset).map(|n| n.to_string())
         },
 
         EventData::Metadata(_) => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::CIP25Asset(_) => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::Mint(_) => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::Collateral {
             tx_id,
             index,
         } => {
-            Ok(Some(tx_id +"#"+&index.to_string()).map(|n| n.to_string()))
+            Some(tx_id +"#"+&index.to_string()).map(|n| n.to_string())
         },
 
         EventData::NativeScript {
             ..
         } => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::PlutusScript {
             ..
         } => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::StakeRegistration {
             ..
         } => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::StakeDeregistration {
             ..
         } => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::StakeDelegation {
             credential,
             pool_hash,
         } => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string() + "|" + &pool_hash))
+            event.context.tx_hash.clone().map(|n| n.to_string() + "|" + &pool_hash)
         },
 
         EventData::PoolRegistration {
             ..
         }=> {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::PoolRetirement {
             pool,
             epoch,
         } => {
-            Ok(Some(pool))
+            Some(pool)
         },
 
         EventData::GenesisKeyDelegation => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::MoveInstantaneousRewardsCert {
             ..
         } => {
-            Ok(event.context.tx_hash.clone().map(|n| n.to_string()))
+            event.context.tx_hash.clone().map(|n| n.to_string())
         },
 
         EventData::RollBack {
             ..
         } => {
-            Ok(event.context.block_number.map(|n| n.to_string()))
+            event.context.block_number.map(|n| n.to_string())
         },
     }
 }
