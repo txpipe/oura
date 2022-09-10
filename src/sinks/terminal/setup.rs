64bc88317fe72e08afa6acfac6cb6afae43926f3
Santiago Carmuega <santiago@carmuega.me>
2022-09-10 16:54:02 -0300

    chore: Use 'wrap' semantic for terminal width

--- src/sinks/terminal/setup.rs
+++ src/sinks/terminal/setup.rs
@@ -14,7 +14,7 @@ const THROTTLE_MIN_SPAN_MILLIS: u64 = 300;
 #[derive(Default, Debug, Deserialize)]
 pub struct Config {
     pub throttle_min_span_millis: Option<u64>,
-    pub terminal_width: Option<u16>
+    pub wrap: Option<bool>,
 }
 
 impl SinkProvider for WithUtils<Config> {
@@ -25,12 +25,11 @@ impl SinkProvider for WithUtils<Config> {
                 .unwrap_or(THROTTLE_MIN_SPAN_MILLIS),
         );
 
-        let terminal_width = self.inner.terminal_width;
-
+        let wrap = self.inner.wrap.unwrap_or(false);
         let utils = self.utils.clone();
 
         let handle = std::thread::spawn(move || {
-            reducer_loop(throttle_min_span, terminal_width, input, utils).expect("terminal sink loop failed");
+            reducer_loop(throttle_min_span, wrap, input, utils).expect("terminal sink loop failed");
         });
 
         Ok(handle)
