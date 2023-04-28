74b18d12cfdc1f0b27456aaf9c989710acbe37f1
Santiago Carmuega <santiago@carmuega.me>
2023-04-27 21:59:41 -0300

    Remove extra example

--- examples/spreadsheet/deps.ts
+++
@@ -1,1 +0,0 @@
-export { Sheets } from "https://googleapis.deno.dev/v1/sheets:v4.ts";
--- examples/spreadsheet/main.ts
+++
@@ -1,6 +0,0 @@
-import { Sheets } from "./deps.ts";
-
-const sheets = new Sheets();
-const sheet = await sheets.spreadsheetsCreate({});
-
-console.log("pepe");
