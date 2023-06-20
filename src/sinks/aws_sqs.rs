0760dae42e877cbb4a646c66fb8eeb4d84aeff56
paulobressan <paulo.bressan@outlook.com>
2023-06-20 17:02:47 -0300

    feat(sinks): migrated sqs sink

---
+++ src/sinks/aws_sqs.rs
@@ -0,0 +1,70 @@
+use gasket::framework::*;
+use serde::Deserialize;
+
+use crate::framework::*;
+
+pub struct Worker {}
+
+#[async_trait::async_trait(?Send)]
+impl gasket::framework::Worker<Stage> for Worker {
+    async fn bootstrap(stage: &Stage) -> Result<Self, WorkerError> {
+        Ok(Self {})
+    }
+
+    async fn schedule(
+        &mut self,
+        stage: &mut Stage,
+    ) -> Result<WorkSchedule<ChainEvent>, WorkerError> {
+        let msg = stage.input.recv().await.or_panic()?;
+        Ok(WorkSchedule::Unit(msg.payload))
+    }
+
+    async fn execute(&mut self, unit: &ChainEvent, stage: &mut Stage) -> Result<(), WorkerError> {
+        let point = unit.point().clone();
+        let record = unit.record().cloned();
+
+        if record.is_none() {
+            return Ok(());
+        }
+
+        let payload = serde_json::to_vec(&serde_json::Value::from(record.unwrap())).or_panic()?;
+
+        stage.ops_count.inc(1);
+        stage.latest_block.set(point.slot_or_default() as i64);
+        stage.cursor.add_breadcrumb(point.clone());
+
+        Ok(())
+    }
+}
+
+#[derive(Stage)]
+#[stage(name = "filter", unit = "ChainEvent", worker = "Worker")]
+pub struct Stage {
+    config: Config,
+    cursor: Cursor,
+
+    pub input: MapperInputPort,
+
+    #[metric]
+    ops_count: gasket::metrics::Counter,
+
+    #[metric]
+    latest_block: gasket::metrics::Gauge,
+}
+
+#[derive(Default, Debug, Deserialize)]
+pub struct Config {}
+
+impl Config {
+    pub fn bootstrapper(self, ctx: &Context) -> Result<Stage, Error> {
+        let stage = Stage {
+            config: self,
+            cursor: ctx.cursor.clone(),
+            ops_count: Default::default(),
+            latest_block: Default::default(),
+            input: Default::default(),
+        };
+
+        Ok(stage)
+    }
+}
