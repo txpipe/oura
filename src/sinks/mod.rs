b0eb51dea01414e5a46a1517d7c741d96543f755
paulobressan <paulo.bressan@outlook.com>
2023-06-23 15:03:03 -0300

    chore(sinks): started to migrate elasticsearch

--- src/sinks/mod.rs
+++ src/sinks/mod.rs
@@ -27,8 +27,8 @@ mod aws_lambda;
 #[cfg(feature = "sink-redis")]
 mod redis;
 
-// #[cfg(feature = "elasticsink")]
-// pub mod elastic;
+#[cfg(feature = "sink-elasticsearch")]
+mod elasticsearch;
 
 // #[cfg(feature = "aws")]
 // pub mod aws_s3;
@@ -62,6 +62,9 @@ pub enum Bootstrapper {
 
     #[cfg(feature = "sink-redis")]
     Redis(redis::Stage),
+
+    #[cfg(feature = "sink-elasticsearch")]
+    ElasticSearch(elasticsearch::Stage),
 }
 
 impl StageBootstrapper for Bootstrapper {
@@ -93,6 +96,9 @@ impl StageBootstrapper for Bootstrapper {
 
             #[cfg(feature = "sink-redis")]
             Bootstrapper::Redis(p) => p.input.connect(adapter),
+
+            #[cfg(feature = "sink-elasticsearch")]
+            Bootstrapper::ElasticSearch(p) => p.input.connect(adapter),
         }
     }
 
@@ -120,6 +126,9 @@ impl StageBootstrapper for Bootstrapper {
 
             #[cfg(feature = "sink-redis")]
             Bootstrapper::Redis(x) => gasket::runtime::spawn_stage(x, policy),
+
+            #[cfg(feature = "sink-elasticsearch")]
+            Bootstrapper::ElasticSearch(x) => gasket::runtime::spawn_stage(x, policy),
         }
     }
 }
@@ -149,6 +158,9 @@ pub enum Config {
 
     #[cfg(feature = "sink-redis")]
     Redis(redis::Config),
+
+    #[cfg(feature = "sink-elasticsearch")]
+    ElasticSearch(elasticsearch::Config),
 }
 
 impl Config {
@@ -176,6 +188,9 @@ impl Config {
 
             #[cfg(feature = "sink-redis")]
             Config::Redis(c) => Ok(Bootstrapper::Redis(c.bootstrapper(ctx)?)),
+
+            #[cfg(feature = "sink-elasticsearch")]
+            Config::ElasticSearch(c) => Ok(Bootstrapper::ElasticSearch(c.bootstrapper(ctx)?)),
         }
     }
 }
