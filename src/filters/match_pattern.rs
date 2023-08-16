98cca15c58e23530d80ad6d3986c4bc2df9d0b75
paulobressan <paulo.bressan@outlook.com>
2023-08-16 11:56:26 -0300

    feat(filters): started match pattern filter

---
+++ src/filters/match_pattern.rs
@@ -0,0 +1,38 @@
+use gasket::framework::*;
+use serde::Deserialize;
+
+use crate::framework::*;
+
+#[derive(Default, Stage)]
+#[stage(name = "filter-match-pattern", unit = "ChainEvent", worker = "Worker")]
+pub struct Stage {
+    pub input: FilterInputPort,
+    pub output: FilterOutputPort,
+
+    #[metric]
+    ops_count: gasket::metrics::Counter,
+}
+
+#[derive(Default)]
+pub struct Worker;
+
+impl From<&Stage> for Worker {
+    fn from(_: &Stage) -> Self {
+        Worker::default()
+    }
+}
+
+gasket::impl_mapper!(|_worker: Worker, stage: Stage, unit: ChainEvent| => {
+    let out = unit.clone();
+    stage.ops_count.inc(1);
+    out
+});
+
+#[derive(Default, Deserialize)]
+pub struct Config {}
+
+impl Config {
+    pub fn bootstrapper(self, _ctx: &Context) -> Result<Stage, Error> {
+        Ok(Stage::default())
+    }
+}
