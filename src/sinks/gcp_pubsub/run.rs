b68799d174746c07a1243a649912471851bb19c5
Paulo Cesar <461084+pocesar@users.noreply.github.com>
2023-12-01 11:06:13 +0000

    feat: add attributes field to GCP pubsub (#719)

--- src/sinks/gcp_pubsub/run.rs
+++ src/sinks/gcp_pubsub/run.rs
@@ -15,15 +15,19 @@ use crate::{
     utils::{retry, Utils},
 };
 
+pub type GenericKV = HashMap<alloc::string::String, alloc::string::String>;
+
 async fn send_pubsub_msg(
     publisher: &Publisher,
     event: &Event,
     ordering_key: &str,
+    attributes: &GenericKV,
 ) -> Result<(), crate::Error> {
     let body = json!(event).to_string();
     let msg = PubsubMessage {
         data: body.into(),
         ordering_key: ordering_key.into(),
+        attributes: attributes.into(),
         ..Default::default()
     };
 
@@ -43,6 +47,7 @@ pub fn writer_loop(
     error_policy: &ErrorPolicy,
     retry_policy: &retry::Policy,
     ordering_key: &str,
+    attributes: &GenericKV,
     utils: Arc<Utils>,
 ) -> Result<(), crate::Error> {
     let rt = tokio::runtime::Builder::new_current_thread()
@@ -58,7 +63,7 @@ pub fn writer_loop(
 
     for event in input.iter() {
         let result = retry::retry_operation(
-            || rt.block_on(send_pubsub_msg(&publisher, &event, ordering_key)),
+            || rt.block_on(send_pubsub_msg(&publisher, &event, ordering_key, attributes)),
             retry_policy,
         );
 
