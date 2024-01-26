7408fd319d8b9d76ecc6288c3a835d3287871b04
Santiago Carmuega <santiago@carmuega.me>
2024-01-26 19:58:46 -0300

    chore: upgrade Pallas to v0.21

--- src/filters/parse_cbor.rs
+++ src/filters/parse_cbor.rs
@@ -5,18 +5,8 @@ use serde::Deserialize;
 
 use pallas::interop::utxorpc as interop;
 use pallas::ledger::traverse as trv;
-use utxorpc::proto::cardano::v1 as u5c;
 
 use crate::framework::*;
-
-fn map_cbor_to_u5c(cbor: &[u8]) -> Result<u5c::Tx, WorkerError> {
-    let tx = trv::MultiEraTx::decode(trv::Era::Babbage, cbor)
-        .or_else(|_| trv::MultiEraTx::decode(trv::Era::Alonzo, cbor))
-        .or_else(|_| trv::MultiEraTx::decode(trv::Era::Byron, cbor))
-        .or_panic()?;
-
-    Ok(interop::map_tx(&tx))
-}
 
 #[derive(Default, Stage)]
 #[stage(name = "filter-parse-cbor", unit = "ChainEvent", worker = "Worker")]
@@ -40,7 +30,8 @@ impl From<&Stage> for Worker {
 gasket::impl_mapper!(|_worker: Worker, stage: Stage, unit: ChainEvent| => {
     let output = unit.clone().try_map_record(|r| match r {
         Record::CborTx(cbor) => {
-            let tx = map_cbor_to_u5c(&cbor)?;
+            let tx = trv::MultiEraTx::decode(&cbor).or_panic()?;
+            let tx = interop::map_tx(&tx);
             Ok(Record::ParsedTx(tx))
         }
         x => Ok(x),
