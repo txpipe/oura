c26a2636931523169e80e04a711bb2415a0fd857
perturbing <thomas.vellekoop@iohk.io>
2024-03-12 09:52:59 +0100

    update flake and explicitly version pallas

--- flake.nix
+++ flake.nix
@@ -1,24 +1,35 @@
 {
+  description = "A Nix flake for building the oura project on the main branch";
+
   inputs = {
-    crane.url = "github:ipetkov/crane";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    self,
-    crane,
-    utils,
-    ...
-  }: let
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux"];
-  in
-    utils.lib.eachSystem supportedSystems
-    (
-      system: {
-        packages.oura = crane.lib.${system}.buildPackage {
-          src = self;
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlay ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-        packages.default = self.packages.${system}.oura;
+      in {
+        packages.oura = pkgs.rustPlatform.buildRustPackage rec {
+          name = "oura";
+          src = self;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "pallas-0.23.0" = "7deb0f9c183c39d24499f123b17372394385a159ee6380df72fc27335cfa28e8"; 
+            };
+          };
+          cargoSha256 = "0000000000000000000000000000000000000000000000000000"; # Placeholder, replace with actual hash
+          buildInputs = with pkgs; [ ];
+        };
+
+        defaultPackage = self.packages.oura;
       }
     );
 }
+
